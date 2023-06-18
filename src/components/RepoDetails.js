11bffc1102cb812a5c7c2b2d1e523e562159df2b
sanjucs12 <sanjucs12@gmail.com>
2023-06-18 13:02:34 +0530

    repo details added

---
+++ src/components/RepoDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RepoDetails = ({ repo }) => {
+  return (
+    <div>
+      <h2>{repo.name}</h2>
+      <p>Author: {repo.owner.login}</p>
+      <p>Language: {repo.language}</p>
+      <p>Stars: {repo.stargazers_count}</p>
+      <p>Forks: {repo.forks_count}</p>
+    </div>
+  );
+};
+
+export default RepoDetails;
