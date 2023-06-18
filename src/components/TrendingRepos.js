0f4cc35ea1edd37853943eb2f8ba52a4a4c4ddc1
sanjucs12 <sanjucs12@gmail.com>
2023-06-18 13:01:26 +0530

    form added

--- src/components/TrendingRepos.js
+++ src/components/TrendingRepos.js
@@ -1,27 +1,53 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Form } from 'react-bootstrap';
 
 const TrendingRepos = () => {
   const [repos, setRepos] = useState([]);
+  const [language, setLanguage] = useState('');
+  const [date, setDate] = useState('');
 
   useEffect(() => {
     const fetchTrendingRepos = async () => {
       try {
-        const response = await axios.get("https://api.github.com/repositories");
+        const response = await axios.get('https://api.github.com/repositories', {
+          params: {
+            language,
+            since: date
+          }
+        });
         setRepos(response.data);
       } catch (error) {
-        console.error("Error fetching trending repositories:", error);
+        console.error('Error fetching trending repositories:', error);
       }
     };
 
     fetchTrendingRepos();
-  }, []);
+  }, [language, date]);
+
+  const handleLanguageChange = event => {
+    setLanguage(event.target.value);
+  };
+
+  const handleDateChange = event => {
+    setDate(event.target.value);
+  };
 
   return (
     <div>
       <h1>Trending GitHub Repositories</h1>
+      <Form>
+        <Form.Group controlId="languageFilter">
+          <Form.Label>Filter by Language:</Form.Label>
+          <Form.Control type="text" placeholder="Enter language" value={language} onChange={handleLanguageChange} />
+        </Form.Group>
+        <Form.Group controlId="dateFilter">
+          <Form.Label>Filter by Date:</Form.Label>
+          <Form.Control type="date" value={date} onChange={handleDateChange} />
+        </Form.Group>
+      </Form>
       <ul>
-        {repos.map((repo) => (
+        {repos.map(repo => (
           <li key={repo.id}>
             <h3>{repo.name}</h3>
             <p>Author: {repo.owner.login}</p>
